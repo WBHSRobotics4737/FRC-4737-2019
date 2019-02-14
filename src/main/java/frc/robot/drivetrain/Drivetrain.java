87b8dc195dd86b0d353c8364ed1d1dc19466d4a9
Brian Semrau <brian.semrau@gmail.com>
2019-02-14 16:36:48 -0500

    Added jerk-limiting to Drive

--- src/main/java/frc/robot/drivetrain/Drivetrain.java
+++ src/main/java/frc/robot/drivetrain/Drivetrain.java
@@ -10,8 +10,11 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 //import edu.wpi.first.wpilibj.command.Subsystem;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import org.graalvm.compiler.nodes.calc.LeftShiftNode;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.libs.JerkLimitedTalonSRXController;
 import frc.robot.RobotMap;
 
 import frc.robot.drivetrain.commands.TeleOpDrive;
@@ -30,6 +33,10 @@ public class Drivetrain extends Subsystem {
 
   private DifferentialDrive drive;
 
+  private JerkLimitedTalonSRXController lSmoothDrive;
+  private JerkLimitedTalonSRXController rSmoothDrive;
+  private DifferentialDrive smoothDrive;
+
   public Drivetrain() {
     frontLeftM = new WPI_TalonSRX(RobotMap.FRONTLEFTM);
     rearLeftS = new WPI_TalonSRX(RobotMap.REARLEFTS);
@@ -40,7 +47,11 @@ public class Drivetrain extends Subsystem {
 	  rearRightS.follow(frontRightM);
 	  rearLeftS.follow(frontLeftM);
     
-	  drive = new DifferentialDrive(frontLeftM, frontRightM);
+    drive = new DifferentialDrive(frontLeftM, frontRightM);
+    
+    smoothDrive = new DifferentialDrive(
+				lSmoothDrive = new JerkLimitedTalonSRXController(frontLeftM, 1, 8, 30),
+				rSmoothDrive = new JerkLimitedTalonSRXController(frontRightM, 1, 8, 30));
   }
   
   @Override
@@ -50,18 +61,14 @@ public class Drivetrain extends Subsystem {
   }
 
   public void arcadeDrive(double throttle, double steer) {
-    drive.arcadeDrive(throttle, steer);
+    // drive.arcadeDrive(throttle, steer);
+    smoothDrive.arcadeDrive(throttle, steer);
   }   
 
   public void setBrakeMode() {
-
-    
 		frontLeftM.setNeutralMode(NeutralMode.Brake);
-
 		rearLeftS.setNeutralMode(NeutralMode.Brake);
-
 		frontRightM.setNeutralMode(NeutralMode.Brake);
-
 		rearRightS.setNeutralMode(NeutralMode.Brake);
   }
 
