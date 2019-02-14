5c7481dbc3d0bec0ad2b0dd3c20652c0806c6a37
Brian Semrau <brian.semrau@gmail.com>
2019-02-14 15:59:28 -0500

    Changed Hatch controls

--- src/main/java/frc/robot/hatch/HatchSub.java
+++ src/main/java/frc/robot/hatch/HatchSub.java
@@ -2,6 +2,7 @@ package frc.robot.hatch;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.RobotMap;
+import frc.robot.hatch.commands.DisableHatch;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.Solenoid;
@@ -9,55 +10,52 @@ import edu.wpi.first.wpilibj.Solenoid;
 
 public class HatchSub extends Subsystem {
 
-    private DoubleSolenoid topsolenoid, bottomsolenoid;
-    private Solenoid singlesolenoid = new Solenoid(10);
+  private DoubleSolenoid topsolenoid, bottomsolenoid;
+  private DoubleSolenoid extendersolenoid;
 
-    public HatchSub() {
-        topsolenoid = new DoubleSolenoid(RobotMap.TOPSOLENOID_FORWARDCHANNEL, RobotMap.TOPSOLENOID_REVERSECHANNEL);   
-        bottomsolenoid = new DoubleSolenoid(RobotMap.BOTTOMSOLENOID_FORWARDCHANNEL, RobotMap.BOTTOMSOLENOID_REVERSECHANNEL);
-        singlesolenoid.set(true);
-        singlesolenoid.set(false);
-      }
+  public HatchSub() {
+    topsolenoid = new DoubleSolenoid(RobotMap.TOPSOLENOID_FORWARDCHANNEL, RobotMap.TOPSOLENOID_REVERSECHANNEL);
+    bottomsolenoid = new DoubleSolenoid(RobotMap.BOTTOMSOLENOID_FORWARDCHANNEL, RobotMap.BOTTOMSOLENOID_REVERSECHANNEL);
+    extendersolenoid = new DoubleSolenoid(RobotMap.SINGLESOLENOID_FORWARDCHANNEL, RobotMap.SINGLESOLENOID_REVERSECHANNEL);
+  }
 
-  
-    public void closePneumatics() {
+  public void extendTop() {
+    topsolenoid.set(Value.kForward);
+  }
 
-      setPneumatics(Value.kReverse, Value.kReverse);
-  
-    }
-  
-  
-  
-    public void openPneumatics() {
-  
-      setPneumatics(Value.kForward, Value.kForward);
-  
-    }
-  
-  
-  
-    public void disablePneumatics() {
-  
-      setPneumatics(Value.kOff, Value.kOff);
-  
-    }
-  
-  
-  
-    public void setPneumatics(DoubleSolenoid.Value top, DoubleSolenoid.Value bottom) {
-  
-      topsolenoid.set(top);
-  
-      bottomsolenoid.set(bottom);
-  
-    }
+  public void retractTop() {
+    topsolenoid.set(Value.kReverse);
+  }
+
+  public void extendBottom() {
+    bottomsolenoid.set(Value.kForward);
+  }
+
+  public void retractBottom() {
+    bottomsolenoid.set(Value.kReverse);
+  }
+
+  public void extendExtender() {
+    extendersolenoid.set(Value.kForward);
+  }
+
+  public void retractExtender() {
+    extendersolenoid.set(Value.kReverse);
+  }
+
+  public void disablePneumatics() {
+    setPneumatics(Value.kOff, Value.kOff);
+  }
+
+  public void setPneumatics(DoubleSolenoid.Value top, DoubleSolenoid.Value bottom) {
+    topsolenoid.set(top);
+    bottomsolenoid.set(bottom);
+  }
+
   @Override
-    public void initDefaultCommand() {
-      // Set the default command for a subsystem here.
-      //setDefaultCommand(new DisableHatch());
-    }
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    setDefaultCommand(new DisableHatch());
+  }
 
 }
-
-
-
---
+++ src/main/java/frc/robot/hatch/commands/SetExtenderHatchPneumatic.java
@@ -0,0 +1,40 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.hatch.commands;
+
+import edu.wpi.first.wpilibj.command.InstantCommand;
+import frc.robot.Robot;
+
+/**
+ * Add your docs here.
+ */
+public class SetExtenderHatchPneumatic extends InstantCommand {
+
+  private boolean extended;
+
+  /**
+   * Add your docs here.
+   */
+  public SetExtenderHatchPneumatic(boolean extended) {
+    super();
+    requires(Robot.HATCH);
+
+    this.extended = extended;
+  }
+
+  // Called once when the command executes
+  @Override
+  protected void initialize() {
+    if (extended) {
+      Robot.HATCH.extendExtender();
+    } else {
+      Robot.HATCH.retractExtender();
+    }
+  }
+
+}
